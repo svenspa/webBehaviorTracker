9a01a1a775ea8744121a3f075d02111c07b6ce05
Dutchometrics <spa.sven@gmail.com>
2021-11-27 15:10:26 +0100

    Saves multiple times

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 url_timestamp = {}
 url_viewtime = {}
 prev_url = ""
+start_time = time.time()
 
 def url_strip(url):
     if "http://" in url or "https://" in url:
@@ -24,6 +25,7 @@ def send_url():
     global url_timestamp
     global url_viewtime
     global prev_url
+    global start_time
 
     if parent_url not in url_timestamp.keys():
         url_viewtime[parent_url] = 0
@@ -35,13 +37,18 @@ def send_url():
     url_timestamp[parent_url] = int(time.time())
     prev_url = parent_url
 
+    if (time.time() - start_time) > 60:
+        start_time = time.time()
+        pd.DataFrame.from_dict(data=url_viewtime, orient='index').rename(columns={0: 'Time in seconds'}).to_csv('dict_file.csv', header=True)
+
+
     return jsonify({'message': 'success!'}), 200
 
 @app.route('/quit_url', methods=['POST'])
 def quit_url():
     resp_json = request.get_data()
     print("Url closed: " + resp_json.decode())
-    pd.DataFrame.from_dict(data=url_viewtime, orient='index').to_csv('dict_file.csv', header=False)
+    pd.DataFrame.from_dict(data=url_viewtime, orient='index').rename(columns={0: 'Time in seconds'}).to_csv('dict_file.csv', header=True)
     return jsonify({'message': 'quit success!'}), 200
 
 app.run(host='0.0.0.0', port=5000)
